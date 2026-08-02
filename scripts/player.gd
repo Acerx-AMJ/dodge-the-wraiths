67548b671d483665945eead7be5f77b12397c507
Daniel Vishnevsky <ajm.acerx@gmail.com>
2026-08-02 21:52:45 +0300

    Changed player.

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,9 +2,16 @@ extends Area2D
 signal hit
 signal enemy_killed
 
-@export var speed = 400
+@export var speed = 300.0
+@export var acceleration = 2500.0
+@export var deceleration = 4000.0
+
 var shielded = false
 var attack_mode = false
+var velocity = Vector2.ZERO
+
+var shadows: Array[Sprite2D]
+var shadow_count = 3
 
 func powerup_shield() -> void:
 	shielded = true
@@ -24,28 +31,40 @@ func powerup_attack_stop() -> void:
 
 func start(pos: Vector2):
 	position = pos
+	for i in range(shadow_count):
+		shadows[i].position = pos
+		shadows[i].show()
+
 	powerup_shield_stop()
 	powerup_attack_stop()
 	show()
 	$CollisionShape2D.disabled = false
 
-func _process(delta: float) -> void:
-	var velocity: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	if velocity.x != 0:
-		$AnimatedSprite2D.animation = "walk"
-		$AnimatedSprite2D.flip_h = velocity.x < 0
-	elif velocity.y != 0:
-		$AnimatedSprite2D.animation = "up"
-		$AnimatedSprite2D.flip_v = velocity.y > 0
+func _ready() -> void:
+	var shadow_alpha_unit = 1.0 / (shadow_count + 1.0)
+	for i in range(shadow_count):
+		var shadow = Sprite2D.new()
+		shadow.position = $Sprite.position
+		shadow.texture = $Sprite.texture
+		shadow.scale = $Sprite.scale - Vector2((i + 1.0) * 0.1, (i + 1.0) * 0.1)
+		shadow.modulate.a = 1.0 - (i + 1.0) * shadow_alpha_unit - shadow_alpha_unit * 0.5
+		shadow.hide()
 
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
-	else:
-		$AnimatedSprite2D.stop()
-	
+		$Shadows.add_child(shadow)
+		shadows.push_back(shadow)
+
+func _process(delta: float) -> void:
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var vel = direction.normalized() * speed
+	var rate = acceleration if (direction.x != 0.0 || direction.y != 0.0) else deceleration
+	velocity = velocity.move_toward(vel, rate * delta)
+
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, get_viewport_rect().size)
+
+	if global_position.distance_squared_to(shadows.back().global_position) > 1.0:
+		for i in range(shadow_count):
+			shadows[i].global_position = shadows[i].global_position.lerp(global_position, 5.0 * delta * (shadow_count - i))
 
 func _on_body_entered(body: Node2D) -> void:
 	if attack_mode:
@@ -53,6 +72,9 @@ func _on_body_entered(body: Node2D) -> void:
 		enemy_killed.emit()
 
 	if not shielded and not attack_mode:
+		for i in range(shadow_count):
+			shadows[i].hide()
+
 		hide()
 		hit.emit()
 		$CollisionShape2D.set_deferred("disabled", true)
